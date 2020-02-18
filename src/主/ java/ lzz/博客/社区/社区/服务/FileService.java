4731a08cf8d6c85f0b4cc9fa82935a8c60617811
程序猿小志 <56374378+LZZ-MZ@users.noreply.github.com>
2020-02-18 17:23:41 +0800

    Update and rename src/main/java/lzz/blog/community/community/service/FileService.java to  src/主/ java/ lzz/博客/社区/社区/服务/FileService.java

--- src/主/ java/ lzz/博客/社区/社区/服务/FileService.java
+++ src/主/ java/ lzz/博客/社区/社区/服务/FileService.java
@@ -21,8 +21,8 @@ import java.util.UUID;
 public class FileService {
 
     // 1 初始化用户身份信息（secretId, secretKey）。
-    private String secretId = "AKID79yZu1oXy32mIhC0rSqzub2EEzUkbY7l";
-    private String secretKey = "hAC0YfbJlxub3GbnkSwBCzWouEKAm7E5";
+    private String secretId = "";
+    private String secretKey = "";
     private COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
     // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
 // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
